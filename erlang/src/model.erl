b3cc02fae780ccebb6c6f7e9a287e92c01675a17
jon.kristensen@nejla.se <jon.kristensen@nejla.se@f40f35a7-fceb-e806-e2e6-912ad0d050cd>
2010-10-25 03:58:20 +0000

    Created an empty Erlang Erlide project.

---
+++ erlang/src/model.erl
@@ -0,0 +1,26 @@
+%% Author: Jon Kristensen
+
+%% Created: Oct 25, 2010
+
+%% Description: This class knows about the current state of the application.
+%%              The GUI queries this class through some kind of Data Access
+%%              Object to know what to draw.
+
+-module(model).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([]).
+
+%%
+%% API Functions
+%%
+
+%%
+%% Local Functions
+%%
