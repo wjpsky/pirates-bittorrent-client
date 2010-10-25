579421ff207fa6c7a334514ad192ad332774100e
jon.kristensen@nejla.se <jon.kristensen@nejla.se@f40f35a7-fceb-e806-e2e6-912ad0d050cd>
2010-10-25 23:13:19 +0000

    Made a gen_server that provides a dummy method for accessing the state of the Erlang part of the program.

--- erlang/src/model.erl
+++ erlang/src/model.erl
@@ -1,26 +1,69 @@
-%% Author: Jon Kristensen
-
-%% Created: Oct 25, 2010
-
-%% Description: This class knows about the current state of the application.
-%%              The GUI queries this class through some kind of Data Access
-%%              Object to know what to draw.
+%% Authors: Jon Kristensen
+%%
+%% Description: This class knows about the current state of the application. The
+%%              GUI queries this class through some kind of Data Access object
+%%              to know what to render.
+%%
+%%              It implements the gen_server behaviour and thus defines a number
+%%              of callbacks used by the gen_server module. See
+%%              http://www.erlang.org/doc/man/gen_server.html for more
+%%              information.
+%%
+%%              We need to start this process from a erl node started with "erl
+%%              -sname 'erlangpbtc@localhost'".
 
 -module(model).
 
-%%
-%% Include files
-%%
+-behaviour(gen_server).
 
-%%
-%% Exported Functions
-%%
--export([]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3, start/0, stop/0, get_state/0]).
 
-%%
-%% API Functions
-%%
 
-%%
-%% Local Functions
-%%
+%% =============================================================================
+%% EXPORTED GEN_SERVER CALLBACKS
+%% =============================================================================
+
+init([]) -> {ok, []}.
+
+handle_call(get_state, _From, State) -> {reply, {}, State}.
+
+handle_cast(stop, State) -> {stop, normal, State}.
+
+handle_info(_Info, State) -> {noreply, State}.
+
+terminate(_Reason, _State) -> ok.
+
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+
+%% =============================================================================
+%% EXPORTED CONVENIENCE FUNCTIONS TO START AND STOP THE SERVER
+%% =============================================================================
+
+start() ->
+	case gen_server:start({local, ?MODULE}, ?MODULE, [], []) of
+		{ok, Pid} ->
+			Pid;
+		{error, Reason} ->
+			{error, Reason}
+	end.
+
+stop() ->
+	case gen_server:cast(?MODULE, stop) of
+		ok ->
+			ok;
+		_ ->
+			{error, stop_error}
+	end.
+
+
+%% =============================================================================
+%% PUBLIC API FUNCTIONS
+%% =============================================================================
+
+get_state() -> gen_server:call(?MODULE, get_state).
+
+
+%% =============================================================================
+%% LOCAL FUNCTIONS
+%% =============================================================================
