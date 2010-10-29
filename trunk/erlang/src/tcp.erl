4ac3cdebea63f9773b24ba644898837f1d37e7de
jon.kristensen@nejla.se <jon.kristensen@nejla.se@f40f35a7-fceb-e806-e2e6-912ad0d050cd>
2010-10-29 18:56:06 +0000

    Helped Niel and Navid to turn their tcp module into a gen_server.

--- trunk/erlang/src/tcp.erl
+++ trunk/erlang/src/tcp.erl
@@ -1,34 +1,94 @@
+%% Authors: Niel Madlani, Navid Ranjbar, Jon Kristensen
+%%
+%% Description: This class connects to a port and sends and receives messages.
+%%
+%%              It implements the gen_server behaviour and thus defines a number
+%%              of callbacks used by the gen_server module. See
+%%              http://www.erlang.org/doc/man/gen_server.html for more
+%%              information.
+%%
+%%              The state is the socket.
+
 -module(tcp).
 
--export([listen/0, send/2, recv/2]).
+-behaviour(gen_server).
 
--define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3, start/0, stop/0, connect/0, send/1, recv/0]).
 
-listen() ->
-    {ok, LSocket} = gen_tcp:listen(6888, ?TCP_OPTIONS),
-    io:format("Connected\n"),
-    spawn(fun() -> accept(LSocket) end).
-		 
-accept(LSocket) ->
-    {ok, Socket} = gen_tcp:accept(LSocket),   
-    spawn(fun() -> loop(Socket) end),
-    accept(LSocket).
-  
-send(Socket, Packet)->
-    ok = gen_tcp:send(Socket, Packet),   
-    loop(Packet),
-    send(Socket, Packet).
-		   
-recv(Socket, Length) ->
-    ok = gen_tcp:recv(Socket, Length),
-    loop(Socket),
-    recv(Socket, Length).
 
-loop(Socket) ->
-    case gen_tcp:recv(Socket, 100) of
-        {ok, Packet} ->
-	    gen_tcp:send(Socket, Packet),
-            loop(Socket);
-        {error, closed} ->
-            io:format("Closed\n")
-    end.
+%% =============================================================================
+%% EXPORTED GEN_SERVER CALLBACKS
+%% =============================================================================
+
+init([]) -> {ok, no_connection}.
+
+handle_call({send, Packet}, _From, State) ->
+	gen_tcp:send(State, Packet),
+	io:format("Send Working\n"),
+	{reply, ok, State};
+
+handle_call(recv, _From, State) ->
+	Message = gen_tcp:recv(State, 0),
+    io:format("~w~n", [Message]),
+	{reply, Message, State}.
+
+handle_cast(connect, _) ->
+    case gen_tcp:listen(6888, [list]) of
+	{ok, LSocket}->
+	    io:format("~w~n", [LSocket]),
+	    case gen_tcp:accept(LSocket) of 
+		{ok, Socket} ->
+		    inet:setopts(Socket, [{active, false}]),
+		    io:format("accepted\n"),
+		    {noreply, Socket};
+		Other ->
+		    io:format("~w~n",[Other])
+	    end;
+	{error, Reason} ->
+	    Reason
+    end;
+
+handle_cast(stop, State) -> {stop, normal, State}.
+
+handle_info(_Info, State) -> {noreply, State}.
+
+terminate(_Reason, _State) -> ok.
+
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+
+%% =============================================================================
+%% EXPORTED CONVENIENCE FUNCTIONS TO START AND STOP THE SERVER
+%% =============================================================================
+
+start() ->
+	case gen_server:start({local, ?MODULE}, ?MODULE, [], []) of
+		{ok, Pid} ->
+			Pid;
+		Reason ->
+			Reason
+	end.
+
+stop() ->
+	case gen_server:cast(?MODULE, stop) of
+		ok ->
+			ok;
+		_ ->
+			{error, stop_error}
+	end.
+
+
+%% =============================================================================
+%% PUBLIC API FUNCTIONS
+%% =============================================================================
+
+connect() -> gen_server:cast(?MODULE, connect).
+
+send(Packet) -> gen_server:call(?MODULE, {send, Packet}).
+
+recv() -> gen_server:call(?MODULE, recv).
+
+
+%% =============================================================================
+%% LOCAL FUNCTIONS
+%% =============================================================================
