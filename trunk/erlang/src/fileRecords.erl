80d91fedd2bb2a1abc21071fe704d74528dc54ee
nenastojova <nenastojova@f40f35a7-fceb-e806-e2e6-912ad0d050cd>
2010-11-02 22:56:55 +0000

    This is the new records module, with the updated simpleParser module, the updates in the simpleParser change the way we extrat the information after info, that is we extract raw data instead of a parsed one. The fileREcords module stores the parsed information into records so that they can be accesed easely

---
+++ trunk/erlang/src/fileRecords.erl
@@ -0,0 +1,68 @@
+%% Author: nenastojova
+%% Created: Nov 2, 2010
+%% Description: TODO: Add description to fileRecords
+-module(fileRecords).
+-export([toRec/1,test/1]).
+-include("torrent_records.hrl"). 
+
+toRec(Data)->
+    toRec(Data,#torrent{}).
+
+toRec([],Tor)->
+    Tor;
+toRec([{announce,Val}|T],Tor) ->
+    toRec(T,Tor#torrent{announce=Val});
+toRec([{info,Val}|T],Tor) ->           
+    toRec(T,Tor#torrent{info=toInfoRec(Val,#torrent_info{})});
+toRec([{rawInfo,Val}|T],Tor)->
+    Hash=hash_info(Val),
+    toRec(T,Tor#torrent{info_hash=Hash});
+toRec([_H|T],Tor) -> 
+    toRec(T,Tor).
+
+toInfoRec([],Info)->
+    Info;
+toInfoRec([{files,Val}|T],Info)->
+     toInfoRec(T,Info#torrent_info{files=fetchFiles(Val,[])});
+toInfoRec([{name,Val}|T],Info) ->
+    toInfoRec(T,Info#torrent_info{name=Val});
+toInfoRec([{'piece length',Val}|T],Info) ->
+     toInfoRec(T,Info#torrent_info{piece_length=Val});
+toInfoRec([{pieces,Val}|T],Info) ->
+    toInfoRec(T,Info#torrent_info{pieces=fetchPieces(Val,1)}).
+
+fetchPieces([],_Key)->
+    [];
+fetchPieces(List,Key) ->
+    {Hash,Rest}=simpleParser:fetchString(List,20,[]),
+    [{Key,Hash}|fetchPieces(Rest,Key+1)].
+
+fetchFiles([],Result)->
+    Result;
+fetchFiles([H|T],Result) ->
+    File=decodeFile(H,#torrent_file{}),
+    fetchFiles(T,[File|Result]).
+
+decodeFile([],Tor)->
+    Tor;
+decodeFile([{length,Len}|T],Tor) ->
+    decodeFile(T,Tor#torrent_file{length=Len});
+decodeFile([{path,Path}|T],Tor) ->
+    decodeFile(T,Tor#torrent_file{path=convertPath(Path)}).
+
+convertPath([])->
+    [];
+convertPath([H|[]])->
+    H;
+convertPath([H|T]) ->
+    H++"/"++convertPath(T).
+
+hash_info(List)->
+    crypto:start(),
+    Hash=crypto:sha(List),
+    crypto:stop(),
+    Hash.
+
+test(Rec)->
+    (Rec#torrent.info)#torrent_info.name,
+    Rec#torrent.info_hash.
---
+++ trunk/erlang/src/torrent_records.erl
@@ -0,0 +1,24 @@
+%% Author: nenastojova
+%% Created: Nov 2, 2010
+%% Description: TODO: Add description to torrent_records
+-module(torrent_records).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([]).
+
+%%
+%% API Functions
+%%
+
+
+
+%%
+%% Local Functions
+%%
+
