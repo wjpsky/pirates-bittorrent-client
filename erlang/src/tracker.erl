522de4be100bf29d9b47502fe6c5b024b84de7b5
mehdi61b <mehdi61b@f40f35a7-fceb-e806-e2e6-912ad0d050cd>
2010-11-23 15:17:40 +0000

    static string correction to communicate tracker it was not in one line

--- erlang/src/tracker.erl
+++ erlang/src/tracker.erl
@@ -12,10 +12,7 @@
 %% this function set an predefined request-which is used for 
 %% the test purpose- then call start(predefined request). 
 start()->
-	PreDefinedRequest= "http://torrent.fedoraproject.org:6969/announce
-    ?info_hash=%9F%A5%3C%C0%4B%16%38%F4%1C%7E%68%43%70%44%6B%34%9D%5F%62%37
-    &peer_id=ABCDEFGHIJKLMNOPQRST&ip=193.11.196.59&port=6881&downloaded=10&
-    left=1588&event=started",
+	PreDefinedRequest= "http://torrent.fedoraproject.org:6969/announce?info_hash=%9F%A5%3C%C0%4B%16%38%F4%1C%7E%68%43%70%44%6B%34%9D%5F%62%37&peer_id=ABCDEFGHIJKLMNOPQRST&ip=193.11.196.59&port=6881&downloaded=10&left=1588&event=started",
 	start(PreDefinedRequest).
 
 %% this function call the inets:start() to have service for using
