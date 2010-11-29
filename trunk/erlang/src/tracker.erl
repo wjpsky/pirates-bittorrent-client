40fd0fe53cb2a85a0bbb35b85eb454f9396499ac
mehdi61b <mehdi61b@f40f35a7-fceb-e806-e2e6-912ad0d050cd>
2010-11-29 10:19:32 +0000

    Added record for peer_info and implement to_record function to convert peer information list to record. Also update comments.

--- trunk/erlang/src/tracker.erl
+++ trunk/erlang/src/tracker.erl
@@ -1,7 +1,19 @@
-%% Author: mahdi abdinejadi
-%% Created: Nov 1, 2010
-%% Description: this module connect to tracker and request for the peers list 
+%%---------------------------------------------------------- 
+%% @author mahdi abdinejadi <mehdi61b@gmail.com> @end
+%% @version 0.1
+%% @since Nov 1, 2010. 
+%% @doc this module connect to tracker and request for the peers list. 
+%% @end
+%%----------------------------------------------------------
+ 
 -module(tracker).
+
+%% peer info
+-record(peer,{
+			  peer_id  :: string(),
+			  ip       :: string(),
+			  port     :: integer()
+			  }).
 
 
 %%
@@ -15,14 +27,27 @@ start()->
 	PreDefinedRequest= "http://torrent.fedoraproject.org:6969/announce?info_hash=%9F%A5%3C%C0%4B%16%38%F4%1C%7E%68%43%70%44%6B%34%9D%5F%62%37&peer_id=ABCDEFGHIJKLMNOPQRST&ip=193.11.196.59&port=6881&downloaded=10&left=1588&event=started",
 	start(PreDefinedRequest).
 
-%% this function call the inets:start() to have service for using
+%% @doc this function call the inets:start() to have service for using
 %% the http module function, then it send a request the url and get
-%% get respond and parse it. 
-start(Request) ->
-    inets:start(),
-    case httpc:request(Request) of
-      {ok, {_, _, Respond}} -> Test = torrent_file_parser:decode(Respond),%io:format("test respond") ;
-			       [{peers, List_peers}, {interval, Interval}, {incomplete, Incomplete}, {complete, Complete}] = Test,
-			       List_peers;
-      Error -> error_logger:error_msg("An error occurred", [Error, ?LINE, ?MODULE]) %,io:format("test error")
-    end.
+%% get respond and parse it. @end
+-spec(start/1 :: (string()) -> term()).
+
+start(Request)->
+	inets:start(), 
+	case httpc:request(Request) of
+		{ok, {_,_,Respond}}-> Test = simpleParser:decode(Respond),%io:format("test respond") ;
+								[{peers,List_peers},{interval,_Interval},{incomplete,_Incomplete},{complete,_Complete}] = Test,
+%% 							  List_peers;
+							  to_record(List_peers, []);
+		Error ->  error_logger:error_msg("An error occurred", [Error,?LINE,?MODULE]) %,io:format("test error")
+	end.
+
+%% @doc this function convert a peer list to a list of peer record list.
+-spec(to_record/2 :: ( term(),term()) -> term()).
+
+to_record([], Acc)->
+	Acc;
+to_record([[{ip,Ip},{_peer_id,Peer_id},{port, Port}]| Rest], Acc)->
+	Peer = #peer{ ip= Ip, peer_id = Peer_id, port= Port },
+	to_record(Rest, [Peer|Acc]).
+
