87ebdb49bf308ebfcb35e572494dc6017a316495
jon.kristensen@nejla.se <jon.kristensen@nejla.se@f40f35a7-fceb-e806-e2e6-912ad0d050cd>
2010-12-10 20:37:34 +0000

    Correct controller file.

--- erlang/src/controller.erl
+++ erlang/src/controller.erl
@@ -13,7 +13,6 @@
 %%
 %% RUNNING THE APPLICATION:
 %%
-%% <modify parse_torrent_file() below>
 %% application:start(sasl).
 %% l(controller).
 %% l(open_file).
@@ -24,9 +23,11 @@
 
 -module(controller).
 
+-include("torrent_records.hrl").
+
 -behaviour(gen_server).
 
--export([init/1,handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3, start/0, stop/0, parse_torrent_file/1,fetchFromQ/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3, start/0, stop/0, parse_torrent_file/0, parse_torrent_file/1]).
 
 
 %% =============================================================================
@@ -34,33 +35,45 @@
 %% =============================================================================
 
 init([]) -> 
-	{ok, {peer_id:get_id(),[]} }.
+	{ok, peer_id:get_id()}.
 
-handle_call(fetchQ, _From, State) ->
-	{Element,NewState}=fetchQ(State),
-	{reply,Element,NewState};
-handle_call(_Request, _From, State) ->
-    {noreply, State}.
+handle_call({parse_torrent_file, File}, _From, State) ->
+    spawn(open_file, start, [File]),
+    {reply, ok, State}.
+
+handle_cast({torrent_file_parsed, ParsedData}, State) ->
+    io:format("Torrent file parsed.\n"),
+    Record = file_records:toRec(ParsedData),
+    spawn(tracker, start, [Record, State]),
+	{noreply, State};
+
+handle_cast({got_peers, Peers, Pieces, PieceLength, PeerID, InfoHash}, State) ->
+    io:format("Spawning downloading FSM...\n"),
+    gen_fsm:start(downloader_fsm, [Peers, Pieces, PieceLength, PeerID, InfoHash], []),
+	{noreply, State};
+
+
+
 
 handle_cast({notify_event, {parse_torrent_file,File}}, State) ->	
 	spawn(open_file,start,[File]),
-	{noreply, addToQ({parse_torrent_file,File},State)};
+	{noreply, State};
 
 handle_cast({notify_event, {torrent_file_content,Data}}, State) ->	
 	spawn(torrent_file_parser,decode,[Data]),
-	{noreply, addToQ({torrent_file_content,Data},State)};
+	{noreply, State};
 
 handle_cast({notify_event, {torrent_file_parsed_data,ParsedData}}, State) ->	
 	spawn(file_records,toRec,[ParsedData]),
-	{noreply, addToQ({torrent_file_parsed_data,ParsedData},State)};
+	{noreply, State};
 
 handle_cast({notify_event, {torrent_record,Rec}}, State) ->	
 	file_records:test(Rec),
-	{noreply, addToQ({torrent_record,Rec},State)};
+	{noreply, State};
 
-handle_cast(stop, State) -> {stop, normal, State};
 
-handle_cast(Request, State) -> {noreply, addToQ(Request,State)}.
+
+handle_cast(stop, State) -> {stop, normal, State}.
 
 handle_info(_Info, State) -> {noreply, State}.
 
@@ -77,10 +90,11 @@ start() ->
 	case gen_server:start({local, ?MODULE}, ?MODULE, [], []) of
 		{ok, Pid} ->
 			%%The logger file is created
-			%logger:create_file("logger.txt"),
+			% was undef:
+			% logger:create_file("logger.txt"),
 			
-			event_manager:start(),
-			event_manager:register(Pid),
+			% event_manager:start(),
+			% event_manager:register(Pid),
 			Pid;
 		{error, Reason} ->
 			error_logger:error_report({"An error occurred", Reason, [{line,?LINE},{module,?MODULE}]})
@@ -89,7 +103,6 @@ start() ->
 stop() ->
 	case gen_server:cast(?MODULE, stop) of
 		ok ->
-			event_manager:stop(),
 			ok;
 		_ ->
 			{error, stop_error}
@@ -100,19 +113,14 @@ stop() ->
 %% PUBLIC API FUNCTIONS
 %% =============================================================================
 
-parse_torrent_file(File) ->
-	event_manager:notify({parse_torrent_file,File}).
+parse_torrent_file() ->
+	parse_torrent_file("asd.torrent").
 
-fetchFromQ()->
-	gen_server:call(?MODULE, fetchQ).
+parse_torrent_file(File) ->
+	% Disabled since it causes the application to crash.
+	% event_manager:notify({parse_torrent_file,File}),
+	gen_server:call(?MODULE, {parse_torrent_file, File}).
 
 %% =============================================================================
 %% LOCAL FUNCTIONS
 %% =============================================================================
-
-addToQ(Element,{ID,Queue})->
-	{ID,Queue++[Element]}.
-fetchQ({ID,[]})->
-	{empty,{ID,[]}};
-fetchQ({ID,[H|T]})->
-	{H,{ID,T}}.
