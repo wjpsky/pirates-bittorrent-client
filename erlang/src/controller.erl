58fd8dfebf6413ab471cd01d81b117dc8f6a3cc3
jon.kristensen@nejla.se <jon.kristensen@nejla.se@f40f35a7-fceb-e806-e2e6-912ad0d050cd>
2010-12-11 07:47:35 +0000

    Cleaned up the controller module.

--- erlang/src/controller.erl
+++ erlang/src/controller.erl
@@ -1,25 +1,34 @@
-%% Authors: Jon Kristensen, Nena Stojova
-%%
-%% Description: This class is the target of events from for example a GUI. It
-%%              makes things happen in the application.
-%%
-%%              It implements the gen_server behaviour and thus defines a number
-%%              of callbacks used by the gen_server module. See
-%%              http://www.erlang.org/doc/man/gen_server.html for more
-%%              information.
-%%
-%%              We need to start this process from a erl node started with "erl
-%%              -sname 'erlangpbtc@localhost'".
-%%
-%% RUNNING THE APPLICATION:
-%%
-%% application:start(sasl).
-%% l(controller).
-%% l(open_file).
-%% l(torrent_file_parser).
-%% controller:start().
-%% controller:parse_torrent_file().
+%%==============================================================================
+%% MODULE DESCRIPTION
+%%==============================================================================
 
+% @author Jon Kristensen
+%     (did everything but the notify_event messages)
+% @author Nena Stojova
+%     (implemented the notify_event messages)
+
+% @doc
+% <p>The controller process is a gen_server which receives events and acts on
+% them. These messages can come from both the Java GUI and other modules. The
+% most important message right now is probably the "RPC" function callable from
+% the Java code, "parse_torrent_file(File)". The other messages are regular
+% Erlang messages, such as {parse_torrent_file, File}, etc.</p>
+% <p>We need to start this process from a erl node started with <code>erl -sname
+% 'erlangpbtc@localhost'</code> in order for it to be able to communicate with
+% the Java code.</p>
+% <p>Below are instructions on how to run this code.</p>
+% <ol>
+% <li><code>application:start(sasl).</code></li>
+% <li><code>l(controller).</code></li>
+% <li><code>l(open_file).</code></li>
+% <li><code>l(torrent_file_parser).</code></li>
+% <li><code>controller:start().</code></li>
+% <li><code>controller:parse_torrent_file([INSERT FILE NAME]).</code></li>
+% </ol>
+
+%%==============================================================================
+%% DEFINITION, INCLUDES, DEFINES, EXPORTS
+%%==============================================================================
 
 -module(controller).
 
@@ -27,33 +36,37 @@
 
 -behaviour(gen_server).
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3, start/0, stop/0, parse_torrent_file/0, parse_torrent_file/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3, start/0, stop/0, parse_torrent_file/1]).
 
+%%==============================================================================
+%% GEN_SERVER CALLBACKS
+%%==============================================================================
 
-%% =============================================================================
-%% EXPORTED GEN_SERVER CALLBACKS
-%% =============================================================================
-
+% Initializes the gen_server, generating a peer ID for this client.
 init([]) -> 
 	{ok, peer_id:get_id()}.
 
+% Handles the parse_torrent_file message. This message is sent from the
+% parse_torrent_file function below. It spawns an open_file process.
 handle_call({parse_torrent_file, File}, _From, State) ->
     spawn(open_file, start, [File]),
     {reply, ok, State}.
 
+% Handles the torrent_file_parsed message. This message is sent from the
+% open_file module. It converts the parsed torrent data into records and spawns
+% the tracker process to begin communicating with the tracker.
 handle_cast({torrent_file_parsed, ParsedData}, State) ->
     io:format("Torrent file parsed.\n"),
     Record = file_records:toRec(ParsedData),
     spawn(tracker, start, [Record, State]),
 	{noreply, State};
 
+% Handles the got_peers message sent from the tracker module. It spawns a
+% download_fsm process to start downloading.
 handle_cast({got_peers, Peers, Pieces, PieceLength, PeerID, InfoHash}, State) ->
     io:format("Spawning downloading FSM...\n"),
     gen_fsm:start(downloader_fsm, [Peers, Pieces, PieceLength, PeerID, InfoHash], []),
 	{noreply, State};
-
-
-
 
 handle_cast({notify_event, {parse_torrent_file,File}}, State) ->	
 	spawn(open_file,start,[File]),
@@ -71,8 +84,6 @@ handle_cast({notify_event, {torrent_record,Rec}}, State) ->
 	file_records:test(Rec),
 	{noreply, State};
 
-
-
 handle_cast(stop, State) -> {stop, normal, State}.
 
 handle_info(_Info, State) -> {noreply, State}.
@@ -82,24 +93,27 @@ terminate(_Reason, _State) -> ok.
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
 
 
-%% =============================================================================
-%% EXPORTED CONVENIENCE FUNCTIONS TO START AND STOP THE SERVER
-%% =============================================================================
+%%==============================================================================
+%% CONVENIENCE FUNCTIONS TO START AND STOP THE SERVER
+%%==============================================================================
 
+% @doc <p>Starts the server. See the module documentation for more details.</p>
+% @spec start() -> {ok, Pid} | {error, Reason}
+-spec start() -> {ok, Pid :: pid()} | {error, Reason :: term()}.
 start() ->
 	case gen_server:start({local, ?MODULE}, ?MODULE, [], []) of
 		{ok, Pid} ->
-			%%The logger file is created
-			% was undef:
-			% logger:create_file("logger.txt"),
-			
-			% event_manager:start(),
-			% event_manager:register(Pid),
 			Pid;
 		{error, Reason} ->
-			error_logger:error_report({"An error occurred", Reason, [{line,?LINE},{module,?MODULE}]})
+			error_logger:error_report({
+				"Could not start the controller gen_server.",
+				Reason}),
+			{error, Reason}
 	end.
 
+% @doc <p>Stops the server. See the module documentation for more details.</p>
+% @spec stop() -> ok | {error, stop_error}
+-spec stop() -> ok | {error, stop_error}.
 stop() ->
 	case gen_server:cast(?MODULE, stop) of
 		ok ->
@@ -108,19 +122,15 @@ stop() ->
 			{error, stop_error}
 	end.
 
+%%==============================================================================
+%% PUBLIC FUNCTIONS 
+%%==============================================================================
 
-%% =============================================================================
-%% PUBLIC API FUNCTIONS
-%% =============================================================================
-
-parse_torrent_file() ->
-	parse_torrent_file("asd.torrent").
-
+% @doc <p>Sends a gen_server message to start parsing the torrent file found at
+% the path <code>File</code>. This function can be called from either the shell
+% or the GUI.
+% @spec parse_torrent_file(File) -> ok
+%     File = string()
+-spec parse_torrent_file(File :: string()) -> ok.
 parse_torrent_file(File) ->
-	% Disabled since it causes the application to crash.
-	% event_manager:notify({parse_torrent_file,File}),
 	gen_server:call(?MODULE, {parse_torrent_file, File}).
-
-%% =============================================================================
-%% LOCAL FUNCTIONS
-%% =============================================================================
