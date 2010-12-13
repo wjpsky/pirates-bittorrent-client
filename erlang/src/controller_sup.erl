362979dc1b194a03ef891ff146a1b7f9da2a50c8
wjpsky@gmail.com <wjpsky@gmail.com@f40f35a7-fceb-e806-e2e6-912ad0d050cd>
2010-12-13 16:28:11 +0000

    controller supervisor

---
+++ erlang/src/controller_sup.erl
@@ -0,0 +1,11 @@
+
+-module(controller_sup).
+-behaviour(supervisor).
+-export([start_link/0, init/1]).
+
+start_link() -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init([]) -> 
+	Child = {controller, {controller, start, []}, permanent, 2000, worker, [controller]},
+	{ok, {{one_for_one, 1, 1}, [Child]}}.
+
