e260654758836e87347880f3d804e7bd0d8cc1df
nielmadlani <nielmadlani@f40f35a7-fceb-e806-e2e6-912ad0d050cd>
2010-12-15 13:37:26 +0000

    added start_link to the downloader_fsm, and it will work now with the supervisor.

--- erlang/src/download_sup.erl
+++ erlang/src/download_sup.erl
@@ -2,9 +2,9 @@
 -behaviour(supervisor).
 -export([start_link/0]).
 -export([init/1]).
-start_link() ->
-    supervisor:start_link(download_sup, []).
+
+start_link() -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) -> 
-	Child = {tracker, {tracker, start_link, []}, permanent, 2000, worker, [tracker]},
+	Child = {downloader_fsm, {downloader_fsm, start_link, []}, permanent, 2000, worker, [downloader_fsm]},
 	{ok, {{one_for_all, 1, 1}, [Child]}}.
