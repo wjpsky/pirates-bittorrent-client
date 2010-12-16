9bb6d697f6904deb7ec29b743f436dbb217cde1a
nenastojova <nenastojova@f40f35a7-fceb-e806-e2e6-912ad0d050cd>
2010-12-16 19:38:56 +0000

    comments added

--- trunk/erlang/src/save_to_file.erl
+++ trunk/erlang/src/save_to_file.erl
@@ -1,10 +1,14 @@
 %% Author: Nena Stojova
 %% Created: Dec 12, 2010
 %% Description: This module receives a pieces of the file and stores them.
+%% This module works on multiple files thus it will find proper file for each piece and proper place(offset) in that file to save
+%% If the piece is partly in one file and other part in the other file, this module handle that.
 %% To run the code you just start the gen_server by calling the start method and pass the torrent record to the start.
 %% Then you need to call save_piece for all the pieces that you have received
 %%
-%% By calling test a dummie torrent file is created which will be used to start saving.
+%% There is a 'test' function provided to test this module. 'test' will start this module by creating a dummy torrent file and continue by passing
+%% all different pieces of the file that supposed to be saved in 2 different files. Pieces defined in a way which one of them should be divided 
+%% into two parts and be written in two files. At the end it, test, will stop the process. 
 
 -module (save_to_file).
 
@@ -39,7 +43,7 @@ open_files([File|T],OpenedFiles)->
 	CreatedFile=create_file(File#torrent_file.path,File#torrent_file.length),
 	open_files(T,[CreatedFile|OpenedFiles]).
 
-
+%% To creat each file in which size it is needed
 create_file(Name,Size)->
 	{ok,File}=file:open(Name,[write]),
 	S=Size*8,
@@ -47,14 +51,10 @@ create_file(Name,Size)->
 	{File,Size}.
 
 
-
-
 % PN: Piece Number
 % PD: Piece Data
 % PSize: Piece Size
 % DSize is size of remaining Data of Piece
-
-
 doSave(_PN,_PD,_DSize,_PSize,_Blocks,_Bytes,[])-> ok;
 doSave(PN,PD,DSize,PSize,Blocks,Bytes,[{File,FSize}|T])->
 	Pos=PSize*(PN-Blocks-1)-Bytes+(PSize-DSize),
@@ -73,6 +73,7 @@ doSave(PN,PD,DSize,PSize,Blocks,Bytes,[{File,FSize}|T])->
 			doSave(PN,PD,DSize,PSize,NewBlocks,NewBytes,T)
 	end.
 
+%% After finding the propper file to save the piece, this function play to do that
 savePieceHere(PN,PD,DSize,PSize,Blocks,Bytes,{File,FSize})->
 	Pos=PSize*(PN-Blocks-1)-Bytes+(PSize-DSize),
 	x({savePieceHere,pn,PN,pd,PD,dsize,DSize,psize,PSize,blocks,Blocks,bytes,Bytes,{File,FSize},pos,Pos}),
@@ -84,12 +85,12 @@ savePieceHere(PN,PD,DSize,PSize,Blocks,Bytes,{File,FSize})->
 			<< PD1:DS1,PD2:DS2 >> = PD,
 			x({pos,Pos,dsize,DSize,fsize,FSize,dsize1,DSize1,dsize2,DSize2,ds1,DS1,ds2,DS2,pd,PD,pd1,binary_to_list(<<PD1:DS1>>),pd2,binary_to_list(<<PD2:DS2>>)});
 		true->
-			x("----------------------"),
 			DS1=DSize*8,DS2=8,<<PD1:DS1>>=PD,<<PD2>>= <<0>>,DSize2=0
 	end,
 	file:pwrite(File,Pos,binary_to_list(<<PD1:DS1>>)),
 	{<<PD2:DS2>>,DSize2}.
 
+%% if the file is not the destination file of the piece we will skip to the next file in the list
 skipToNext(Blocks,Bytes,PSize,FSize)->
 	AllPassed=Blocks*PSize+Bytes+FSize,
 	x({passing,AllPassed,bytes}),
@@ -97,6 +98,7 @@ skipToNext(Blocks,Bytes,PSize,FSize)->
 	NewBytes=AllPassed rem PSize,
 	{NewBlocks,NewBytes}.
 
+%% These functions provided to test only [test/0,xstart/0,xstop/0,x/1]
 test()->
 	xstart(),
 	xstop().
@@ -106,9 +108,9 @@ xstart()->
 	Files=[F1,F2],
 	Info=#torrent_info{name="Erlang User Conference",pieces="no pieces there yet",piece_length=25,files=Files},
 	Tor=#torrent{announce="www.announce",info_hash="1234567890",info=Info},
-	% x(Tor#torrent.info#torrent_info.files),
+	x(Tor#torrent.info#torrent_info.files),
 	% Tor#torrent.info#torrent_info.files.
-	% x(["Tor generated: ",Tor]),
+	x(["Tor generated: ",Tor]),
 	start(Tor).
 xstop()->
 	save_piece(5,list_to_binary("ound the globe.The confer")),
@@ -122,8 +124,12 @@ xstop()->
 	save_piece(6,list_to_binary("ence saw an increase of o")),
 	gen_server:cast(?MODULE,stop).
 
+%% this function used to print out runtime debugging information
+%% for released version one line has been commented
+%% to debug only remove comment signs '%%'
 x(S)->
-	io:format("~p~n",[S]).
+	%% io:format("~p~n",[S]),
+	ok.
 
 
 %%==============================================================================
